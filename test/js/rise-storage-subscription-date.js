47a799dbc3b82dc65f9107cca87f78cd06fe6015
Donna Peplinskie <donna.peplinskie@risevision.com>
2016-08-31 16:07:27 -0400

    Merge code from web-component-rise-storage

---
+++ test/js/rise-storage-subscription-date.js
@@ -0,0 +1,2 @@
+var expiredStatus = {"authorized":false,"expiry":null,"signatures":null,"error":{"message":"Subscription is not active."}},
+    subscribedStatus = {"authorized":true,"expiry":"2016-04-26T08:17:59.059+0000","signatures":null,"error":null};
